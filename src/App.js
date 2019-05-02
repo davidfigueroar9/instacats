e5f3674c256192fe0bee3b3d1fc22d207b77b036
Gisselle <gissellediaz28@gmail.com>
2019-05-02 18:41:08 -0300

    created home footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/header';
 import Counter from './components/counter';
+import Footer from './components/footer';
 import './App.css';
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
      <Header title="Instacats"/>
+     <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './styles.css';
+
+const Footer = (props) => {
+  return (
+    <div className="Footer-container">
+      <div className="Footer">
+        <div className="Icon">
+          <i className="fas fa-home"/>
+        </div>
+        <div className="Icon">
+          <i className="fas fa-search"/>
+        </div>
+        <div className="Icon">
+          <i className="far fa-plus-square"/>
+        </div>
+        <div className="Icon">
+          <i className="far fa-heart"/>
+        </div>
+        <div className="Icon">
+          <i className="fas fa-user"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+  
+  export default Footer;
+
+  
+
+  
