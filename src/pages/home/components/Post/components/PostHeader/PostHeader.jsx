a7892b02f3347cf54d414e0e10fe28fce2a028b2
Gisselle <gissellediaz28@gmail.com>
2019-05-25 14:20:25 -0300

    added PostLikes component

--- src/pages/home/components/Post/components/PostHeader/PostHeader.jsx
+++ src/pages/home/components/Post/components/PostHeader/PostHeader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { UserImage } from '../../../../components';
+import { UserImage } from '../../../../../../components';
 import './styles.css';
 
 const PostHeader = ({ img, name }) => (
